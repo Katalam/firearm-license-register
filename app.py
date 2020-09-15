d50774c301354ad9cfe72dc6cca1299d4bbe0afd
Katalam <b_goerss@icloud.com>
2020-09-15 13:14:43 +0200

    Improved user authorization

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import mysql.connector
 from dotenv import load_dotenv
 import secrets
 import hashlib
+import requests
 load_dotenv()
 
 app = Flask(__name__)
-app.config["APPLICATION_ROOT"] = os.getenv("ROOT")
 
 db = mysql.connector.connect(
     host = os.getenv("HOST"),
@@ -49,7 +49,7 @@ def page_login():
 def page_edit():
     session = get_session(request)
     if not session:
-        return redirect(url_for("login"))
+        return redirect(url_for("page_login"))
     c.execute("SELECT * FROM characters;")
 
     return render_template("edit.html",
@@ -59,7 +59,7 @@ def page_edit():
 def page_logout():
     session = get_session(request)
     if session is None:
-        return redirect(url_for("login"))
+        return redirect(url_for("page_login"))
     result = remove_session(session[0])
     response = redirect("login")
     response.set_cookie(key = "session", value = result[0], max_age = result[1], httponly = True)
@@ -69,25 +69,87 @@ def page_logout():
 def page_edit_char(charId):
     session = get_session(request)
     if session is None:
-        return redirect(url_for("login"))
+        return redirect(url_for("page_login"))
     c.execute("SELECT first_name, last_name, a, b FROM characters WHERE id = %(charid)s;", {"charid": charId})
     data = c.fetchone()
     if data is None:
-        return redirect(url_for("login"))
-    return render_template("edit_char.html", a = data[2], b = data[3], id = charId, h = data[0] + " " + data[1])
+        return redirect(url_for("page_login"))
+    return render_template("edit_char.html",
+                        a = data[2], b = data[3],
+                        id = charId, h = data[0] + " " + data[1],
+                        disable_a = allowed_to_change(data[2], session[2]),
+                        disable_b = allowed_to_change(data[3], session[2]))
 
 @app.route("/character/<charId>/save", methods = ["GET", "POST"])
 def page_edit_char_save(charId):
     session = get_session(request)
     if session is None:
-        return redirect(url_for("login"))
-    try:
-        a = request.form["a"]
-        b = request.form["b"]
-    except KeyError:
-        abort(400)
-    c.execute("UPDATE `characters` SET `a`= %(a)s, `b`= %(b)s WHERE id = %(id)s;", {"a": a, "b": b, "id": charId})
+        return redirect(url_for("page_login"))
+    a = request.form.get("a")
+    b = request.form.get("b")
+
+    c.execute("SELECT * FROM characters WHERE id = %(id)s;", {"id": charId})
+    data = c.fetchone()
+    name = data[1] + " " + data[2]
+    old_a = data[4]
+    old_b = data[5]
+    if not a == old_a:
+        message_discord(session[2], "A", name, get_name_type(old_a), get_name_type(a))
+    if not b == old_b:
+        message_discord(session[2], "B", name, get_name_type(old_b), get_name_type(b))
+
+    if not a is None:
+        c.execute("UPDATE `characters` SET `a`= %(a)s WHERE id = %(id)s;", {"a": a, "id": charId})
+    if not b is None:
+        c.execute("UPDATE `characters` SET `b`= %(b)s WHERE id = %(id)s;", {"b": b, "id": charId})
     return redirect(url_for("page_edit_char", charId = charId))
+
+@app.route("/character/new", methods = ["GET", "POST"])
+def page_new_char():
+    message_discord(1, "A", "Markus Ritter", "Ja", "Sperre")
+    return render_template("base.html")
+
+def message_discord(user, type, name, old_value, new_value):
+    requests.post("https://discordapp.com/api/webhooks/754321362343952475/IAzRcNh-UZdzp65qMhzgbxmdqPBSLXDVeEDTtddfp9m46tBrmUgiiuBHPB8Ndtxv54DI", json={
+        "embeds": [
+            {
+            "title": "Änderung",
+            "description": str(user) + " hat Waffenschein " + type + " von " + name + " von " + old_value + " in " + new_value + " geändert.",
+            "color": 16711680
+            }
+        ],
+        "username": "Waffenscheinregister",
+        "avatar_url": "https://i.imgur.com/f7yTuA5.jpeg"
+        })
+
+def get_name_type(value):
+    switcher = {
+        0: "Nicht Erworben",
+        1: "Entzogen",
+        2: "Sperre",
+        3: "Beantragt",
+        4: "Aktiv"
+    }
+    return switcher.get(value, "Nicht Erworben")
+
+"""
+Return allowed to changed a or b value based on userId
+if returned value is 0 you are not allowed to change it
+"""
+def allowed_to_change(value, userId):
+    c.execute("SELECT police, store FROM users WHERE id = %(userId)s;", {"userId": userId})
+    data = c.fetchone()
+    if data is None:
+        return False
+    switcher = {
+        0: data[0] or data[1], # nicht erworben
+        1: data[1], # Entzogen
+        2: data[0], # Sperre
+        3: data[1], # Beantragt, not available for class b
+        4: data[0] # aktiv
+    }
+    return switcher.get(value, 0)
+        
 
 """
 Removes session from db and returns empty session cookie values.
@@ -181,7 +243,7 @@ Init database for first usage.
 def init_db():
     try:
         c.execute("CREATE TABLE IF NOT EXISTS users (id SERIAL UNIQUE, username VARCHAR(254) NOT NULL UNIQUE, \
-            salt TEXT NOT NULL, hash TEXT NOT NULL, PRIMARY KEY(username))")
+            salt TEXT NOT NULL, hash TEXT NOT NULL, police BOOLEAN, store BOOLEAN, PRIMARY KEY(username))")
         c.execute("CREATE TABLE IF NOT EXISTS sessions (id SERIAL, session_id VARCHAR(254) NOT NULL UNIQUE, expires_after TIMESTAMP NOT NULL, \
             user_id INTEGER REFERENCES users(id), PRIMARY KEY(session_id));")
         c.execute("CREATE TABLE IF NOT EXISTS characters (id SERIAL, first_name VARCHAR(254) NOT NULL, last_name VARCHAR(254) NOT NULL, \
