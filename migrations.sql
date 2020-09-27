26d9fadafe7a9f4573594d28f98523b8d56a1a86
Katalam <39590058+Katalam@users.noreply.github.com>
2020-09-28 00:41:39 +0200

    Added basic migrations and added placehholder for safari workaround

--- migrations.sql
+++ migrations.sql
@@ -3,3 +3,6 @@ ALTER TABLE `characters` ADD `id_card` VARCHAR(254) NOT NULL AFTER `birthday`;
 ALTER TABLE `characters` ADD `comment` VARCHAR(254) NOT NULL AFTER `b`,  
 ALTER TABLE `characters` ADD `last_edited` VARCHAR(254) NOT NULL AFTER `comment`;
 ALTER TABLE `characters` ADD `last_edited_from` INT(11) NOT NULL AFTER `last_edited`;
+ALTER TABLE `characters` ADD `medic_assessment` VARCHAR(254) NOT NULL AFTER `last_edited_from`;
+ALTER TABLE `characters` ADD `medic_assessment_edited` VARCHAR(254) NOT NULL AFTER `medic_assessment`;
+ALTER TABLE `characters` ADD `medic_assessment_edited_from` VARCHAR(254) NOT NULL AFTER `medic_assessment_edited`;
